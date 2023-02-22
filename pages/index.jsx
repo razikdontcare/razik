e2da8f42042b0a72de7a7a1756508606a20717a2
Razik <GamerSia4@gmail.com>
2023-02-22 06:10:26 +0000

    added footer

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Head from "next/head";
-import {AiOutlineLink} from 'react-icons/ai'
+import { AiOutlineLink } from "react-icons/ai";
+import {BsInstagram} from 'react-icons/bs'
 
 function Home() {
   return (
@@ -30,7 +31,10 @@ function Home() {
             18 year old web developer. based in Lombok, Indonesia. currently
             studying at SMAN 1 Gerung as 3rd grade student.
           </p>
-          <a href="mailto:contact@razik.my.id" className="font-bold text-lg text-cyan-600">
+          <a
+            href="mailto:contact@razik.my.id"
+            className="font-bold text-lg text-cyan-600"
+          >
             Contact Me!
           </a>
         </div>
@@ -38,18 +42,46 @@ function Home() {
         <div className="divider"></div>
 
         <div className="mt-5">
-          <h2 className="font-bold text-xl">
-            Latest Project
-          </h2>
+          <h2 className="font-bold text-xl">Latest Project</h2>
           <div className="p-2 grid gap-2">
-            <a href="https://sptfy.tech" className="link link-hover text-lg flex font-semibold items-center">
+            <a
+              href="https://sptfy.tech"
+              className="link link-hover text-lg flex font-semibold items-center"
+            >
               <AiOutlineLink className="mr-1" />
               sptfy.tech
             </a>
-            <p className="text-sm">sptfy.tech is Spotify URL Shortener to make your long Spotify link into a short link that you can put on your Social Media bio.</p>
-            <img src="/sptfytech.png" alt="sptfy.tech" className="aspect-video rounded-xl border-2" />
+            <p className="text-sm">
+              sptfy.tech is Spotify URL Shortener to make your long Spotify link
+              into a short link that you can put on your Social Media bio.
+            </p>
+            <div className="mockup-window border bg-base-300">
+              <img
+                src="/sptfytech.png"
+                alt="sptfy.tech"
+                className="aspect-video"
+              />
+            </div>
+            <a
+              href="/projects"
+              className="font-medium link link-hover text-cyan-700 mt-5"
+            >
+              more projects...
+            </a>
           </div>
         </div>
+
+        <div className="divider"></div>
+
+        <footer className="footer footer-center p-10 text-base-content rounded">
+          <div>
+            <div className="grid grid-flow-col gap-4">
+              <a className="w-[50px]" href="https://www.instagram.com/razikdontcare">
+                <BsInstagram size={70} />
+              </a>
+            </div>
+          </div>
+        </footer>
       </div>
     </>
   );
