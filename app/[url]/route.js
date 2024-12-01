11be7277e943d72d33200809ecca93a5e853560e
razikdontcare <gamersia4@gmail.com>
2024-12-02 02:53:33 +0800

    feat: implement dynamic URL redirection in route handler

---
+++ app/[url]/route.js
@@ -0,0 +1,29 @@
+import { redirect } from "next/navigation";
+
+async function getTargetUrl(id) {
+  try {
+    const response = await fetch(`${process.env.BASE_URL}/api/read?id=${id}`, {
+      cache: "no-store",
+    });
+    if (!response.ok) {
+      throw new Error("Failed to fetch target URL");
+    }
+    const data = await response.json();
+    return data.target;
+  } catch (error) {
+    console.error("Error fetching target URL:", error);
+    return null;
+  }
+}
+
+export async function GET(request, { params }) {
+  const targetUrl = await getTargetUrl(params.url);
+
+  if (targetUrl) {
+    return redirect(targetUrl);
+  } else {
+    return new Response("URL Not Found. Please check the URL and try again.", {
+      status: 404,
+    });
+  }
+}
